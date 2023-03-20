d3779ae67b084f388d1a723b65fe64abe7e505d9
yicheng55 <l5977951@gmail.com>
2023-03-20 23:22:16 +0800

    Test code.

--- src/pages/products/index.jsx
+++ src/pages/products/index.jsx
@@ -1,3 +1,7 @@
+import "./single.scss";
+import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+
 import {
   Button,
   EditableText,
@@ -146,154 +150,163 @@ function Products_test() {
   };
 
   return (
-    <div className="App">
-      <table className="bp4-html-table .modifier">
-        <thead>
-          <tr>
-            <th>ID</th>
-            <th>User</th>
-            <th>Item</th>
-            <th>Type</th>
-            <th>Location</th>
-            <th>Tag</th>
-            <th>Quantity</th>
-            <th>Device</th>
-            <th>Physical</th>
-            <th>Comment</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {employees.map((employee) => {
-            // 抓取所需顯示欄位
-            const { ID, USER_ID, ITEM_ID, TYPE, LOCATION_ID, TAG_ID, QUANTITY, DEVICE_ID, PHYSICAL_PORT, COMMENT } = employee;
-            // console.log(employee);
-            // 儲位代號id轉為儲位名稱顯示.
-            let Location = locations.filter(function(elem, index, arr) {
-              return elem.ID === LOCATION_ID;
-            });
-            // console.log(location);
-            // console.log(Location);
-            return (
-              <tr key={ID}>
-                <td>{ID}</td>
-                <td>{USER_ID}</td>
-                <td>{ITEM_ID}</td>
-                <td>{TYPE}</td>
-                <td>{Location[0].NAME}</td>
-                <td>{TAG_ID}</td>
-                <td>{QUANTITY}</td>
-                <td>{DEVICE_ID}</td>
-                <td>{PHYSICAL_PORT}</td>
-                <td>
-                  <EditableText
-                    value={COMMENT}
-                    onChange={(value) => onChangeHandler(ID, "COMMENT", value)}
-                  />
-                </td>
-                <td>
-                  <Button intent="primary" onClick={() => updateAddress(ID)}>
-                    Update
-                  </Button>
-                  &nbsp;
-                  <Button intent="danger" onClick={() => deleteEmployee(ID)}>
-                    Delete
-                  </Button>
-                </td>
-              </tr>
-            );
-          })}
-        </tbody>
-        <tfoot>
-          <tr>
-            <td></td>
-            <td>
-              <InputGroup
-                placeholder="User"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-            </td>
-            <td>
-              <InputGroup
-                placeholder="Item"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-            </td>
-            <td>
-              <InputGroup
-                placeholder="Type"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-            </td>
-            <td>
-              <div className="bp4-html-select .modifier">
-                <select
-                  onChange={(e) => setNewDepartment(e.target.value)}
-                  value={newDepartment}
-                >
-                  <option defaultValue ="none">
-                    Select location
-                  </option>
-                  {locations.map((location) => {
+    <div className="single">
+      <Sidebar />
+      <div className="singleContainer">
+        <Navbar />
+        <div className="top">
+          <div className="left">
 
+              <table className="bp4-html-table .modifier">
+                <thead>
+                  <tr>
+                    <th>ID</th>
+                    <th>User</th>
+                    <th>Item</th>
+                    <th>Type</th>
+                    <th>Location</th>
+                    <th>Tag</th>
+                    <th>Quantity</th>
+                    <th>Device</th>
+                    <th>Physical</th>
+                    <th>Comment</th>
+                    <th>Action</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {employees.map((employee) => {
                     // 抓取所需顯示欄位
-                    const { ID, NAME } = location;
+                    const { ID, USER_ID, ITEM_ID, TYPE, LOCATION_ID, TAG_ID, QUANTITY, DEVICE_ID, PHYSICAL_PORT, COMMENT } = employee;
+                    // console.log(employee);
+                    // 儲位代號id轉為儲位名稱顯示.
+                    let Location = locations.filter(function(elem, index, arr) {
+                      return elem.ID === LOCATION_ID;
+                    });
+                    // console.log(location);
+                    // console.log(Location);
                     return (
-                      <option key={ID} value={ID}>
-                        {NAME}
-                      </option>
+                      <tr key={ID}>
+                        <td>{ID}</td>
+                        <td>{USER_ID}</td>
+                        <td>{ITEM_ID}</td>
+                        <td>{TYPE}</td>
+                        <td>{Location[0].NAME}</td>
+                        <td>{TAG_ID}</td>
+                        <td>{QUANTITY}</td>
+                        <td>{DEVICE_ID}</td>
+                        <td>{PHYSICAL_PORT}</td>
+                        <td>
+                          <EditableText
+                            value={COMMENT}
+                            onChange={(value) => onChangeHandler(ID, "COMMENT", value)}
+                          />
+                        </td>
+                        <td>
+                          <Button intent="primary" onClick={() => updateAddress(ID)}>
+                            Update
+                          </Button>
+                          &nbsp;
+                          <Button intent="danger" onClick={() => deleteEmployee(ID)}>
+                            Delete
+                          </Button>
+                        </td>
+                      </tr>
                     );
                   })}
-                </select>
-                <span className="bp4-icon bp4-icon-double-caret-vertical"></span>
-              </div>
-            </td>
-            <td>
-              <InputGroup
-                placeholder="Tag"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-            </td>
-            <td>
-              <InputGroup
-                placeholder="Quantity"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-            </td>
-            <td>
-              <InputGroup
-                placeholder="Device"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-            </td>
-            <td>
-              <InputGroup
-                placeholder="Physical"
-                value={newName}
-                onChange={(e) => setNewName(e.target.value)}
-              />
-            </td>
-            <td>
-              <InputGroup
-                placeholder="Comment"
-                value={newAddress}
-                onChange={(e) => setNewAddress(e.target.value)}
-              />
-            </td>
-            <td>
-              <Button intent="success" onClick={addEmployee}>
-                Add
-              </Button>
-            </td>
-          </tr>
-        </tfoot>
-      </table>
+                </tbody>
+                <tfoot>
+                  <tr>
+                    <td></td>
+                    <td>
+                      <InputGroup
+                        placeholder="User"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <InputGroup
+                        placeholder="Item"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <InputGroup
+                        placeholder="Type"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <div className="bp4-html-select .modifier">
+                        <select
+                          onChange={(e) => setNewDepartment(e.target.value)}
+                          value={newDepartment}
+                        >
+                          <option defaultValue ="none">
+                            Select location
+                          </option>
+                          {locations.map((location) => {
+
+                            // 抓取所需顯示欄位
+                            const { ID, NAME } = location;
+                            return (
+                              <option key={ID} value={ID}>
+                                {NAME}
+                              </option>
+                            );
+                          })}
+                        </select>
+                        <span className="bp4-icon bp4-icon-double-caret-vertical"></span>
+                      </div>
+                    </td>
+                    <td>
+                      <InputGroup
+                        placeholder="Tag"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <InputGroup
+                        placeholder="Quantity"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <InputGroup
+                        placeholder="Device"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <InputGroup
+                        placeholder="Physical"
+                        value={newName}
+                        onChange={(e) => setNewName(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <InputGroup
+                        placeholder="Comment"
+                        value={newAddress}
+                        onChange={(e) => setNewAddress(e.target.value)}
+                      />
+                    </td>
+                    <td>
+                      <Button intent="success" onClick={addEmployee}>
+                        Add
+                      </Button>
+                    </td>
+                  </tr>
+                </tfoot>
+              </table>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
